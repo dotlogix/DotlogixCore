86c381531cfef9e738bd898861087dca7e8558af
Alexander Schill <alexander@schillnet.de>
2020-08-19 00:02:58 +0200

    Adding naming strategies

---
+++ Infrastructure.EntityFramework/Conventions/DbNamingStrategyBase.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using DotLogix.Core.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DotLogix.Architecture.Infrastructure.EntityFramework.Conventions {
+    public abstract class DbNamingStrategyBase : IDbNamingStrategy {
+        protected char Delimiter { get; } = '_';
+
+        public virtual string GetSchemaName(IEntityType entityType) {
+            var attribute = entityType.ClrType.GetCustomAttribute<TableAttribute>();
+            return RewriteSchemaName(attribute?.Schema);
+        }
+
+        public virtual string GetTableName(IEntityType entityType) {
+            var attribute = entityType.ClrType.GetCustomAttribute<TableAttribute>();
+            var typeName = attribute?.Name ?? entityType.ClrType.Name;
+
+            return RewriteTableName(typeName);
+        }
+
+        public virtual string GetColumnName(IProperty property) {
+            var attribute = property.PropertyInfo.GetCustomAttribute<ColumnAttribute>();
+            return RewriteColumnName(attribute?.Name ?? property.PropertyInfo.Name);
+        }
+
+        public virtual string GetKeyName(IKey key) {
+            var tableName = GetTableName(key.DeclaringEntityType);
+            var columnNames = key.Properties.Select(GetColumnName);
+
+            var isPrimary = key.IsPrimaryKey();
+            var sb = new StringBuilder(isPrimary ? "pk" : "key");
+            sb.Append(Delimiter)
+              .Append(tableName)
+              .Append(Delimiter, 2)
+              .AppendJoin(Delimiter, columnNames);
+
+            return sb.ToString();
+        }
+
+        public virtual string GetForeignKeyName(IForeignKey foreignKey) {
+            var sourceTableName = GetTableName(foreignKey.DeclaringEntityType);
+            var targetTableName = GetTableName(foreignKey.PrincipalEntityType);
+            var sourceColumnNames = foreignKey.Properties.Select(GetColumnName);
+
+            var sb = new StringBuilder("fk");
+            sb.Append(Delimiter)
+              .Append(sourceTableName)
+              .Append(Delimiter, 2)
+              .Append(targetTableName)
+              .Append(Delimiter, 2)
+              .AppendJoin(Delimiter, sourceColumnNames);
+
+            return sb.ToString();
+        }
+
+        public virtual string GetIndexName(IIndex index) {
+            var tableName = GetTableName(index.DeclaringEntityType);
+            var columnNames = index.Properties.Select(GetColumnName);
+
+            var sb = new StringBuilder(index.IsUnique ? "uniq" : "idx");
+            sb.Append(Delimiter)
+              .Append(tableName)
+              .Append(Delimiter, 2)
+              .AppendJoin(Delimiter, columnNames);
+
+            return sb.ToString();
+        }
+
+        public abstract string Rewrite(string value);
+
+        public virtual string RewriteSchemaName(string schemaName) {
+            return Rewrite(schemaName);
+        }
+
+        public virtual string RewriteTableName(string tableName) {
+            return Rewrite(tableName);
+        }
+
+        public virtual string RewriteColumnName(string columnName) {
+            return Rewrite(columnName);
+        }
+    }
+}
--- Infrastructure.EntityFramework/EntityContext/IEfEntityContext.cs
+++ Infrastructure.EntityFramework/EntityContext/IEfEntityContext.cs
@@ -1,30 +1,29 @@
 ﻿using System;
 using DotLogix.Architecture.Infrastructure.EntityContext;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace DotLogix.Architecture.Infrastructure.EntityFramework.EntityContext {
     /// <summary>
-    /// An interface to represent an entity framework entity context
+    ///     An interface to represent an entity framework entity context
     /// </summary>
     public interface IEfEntityContext : IEntityContext {
         /// <summary>
-        /// The current <see cref="DbContext"/>
+        ///     The current <see cref="DbContext" />
         /// </summary>
         DbContext DbContext { get; }
 
         /// <summary>
-        /// The global hooks to the ef store system
+        ///     The global hooks to the ef store system
         /// </summary>
         EfEntityHooks GlobalHooks { get; }
 
         /// <summary>
-        /// Gets the event hooks for a specific entity type
+        ///     Gets the event hooks for a specific entity type
         /// </summary>
         EfEntityHooks GetEntityHooks<TEntity>() where TEntity : class, new();
 
         /// <summary>
-        /// Gets the event hooks for a specific entity type
+        ///     Gets the event hooks for a specific entity type
         /// </summary>
         EfEntityHooks GetEntityHooks(Type entityType);
     }

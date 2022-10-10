8822f88e9718c718c27077b8f0078b13362339f3
Alexander Schill <alexander@schillnet.de>
2022-10-10 15:56:14 +0200

    Fix cache implementation after history rewrite

--- Core/Core/Caching/CacheItem.cs
+++ Core/Core/Caching/CacheItem.cs
@@ -8,52 +8,76 @@
 
 using System.Collections.Generic;
 using System.Linq;
-#if NETSTANDARD2_1
-using System.Linq;
-#else
-#endif
 
+namespace DotLogix.Core.Caching; 
 
-namespace DotLogix.Core.Caching {
+/// <summary>
+///     An item for a cache implementation
+/// </summary>
+/// <typeparam name="TKey"></typeparam>
+/// <typeparam name="TValue"></typeparam>
+public class CacheItem<TKey, TValue> {
+    private IDictionary<object, object> _associatedValues;
+
     /// <summary>
-    ///     An item for a cache implementation
+    ///     The key
     /// </summary>
-    /// <typeparam name="TKey"></typeparam>
-    /// <typeparam name="TValue"></typeparam>
-    public class CacheItem<TKey, TValue> {
-        /// <summary>
-        /// The key
-        /// </summary>
-        public TKey Key { get; }
+    public TKey Key { get; }
 
-        /// <summary>
-        /// The value
-        /// </summary>
-        public TValue Value { get; }
+    /// <summary>
+    ///     The value
+    /// </summary>
+    public TValue Value { get; set; }
 
-        /// <summary>
-        /// The policy to check validity
-        /// </summary>
-        public bool HasChildren => _children != null && _children.Count > 0;
-        /// <summary>
-        ///     Check if there are dependent cache keys
-        /// </summary>
-        public bool HasAssociatedValues => _associatedValues != null && _associatedValues.Count > 0;
-        /// <summary>
-        ///     The dependent keys
-        /// </summary>
-        public HashSet<TKey> Children => _children ?? (_children = new HashSet<TKey>());
+    private HashSet<TKey> _children;
 
-        /// <summary>
-        /// Creates a new instance of <see cref="CacheItem{TKey,TValue}"/>
-        /// </summary>
-        /// <param name="key">The key</param>
-        /// <param name="value">The value</param>
-        /// <param name="policy">The policy to check validity</param>
-        public CacheItem(TKey key, TValue value, ICachePolicy policy) {
-            Key = key;
-            Value = value;
-            Policy = policy;
-        }
+    /// <summary>
+    ///     Check if there are dependent cache keys
+    /// </summary>
+    public bool HasChildren => _children is { Count: > 0 };
+    /// <summary>
+    ///     Check if there are dependent cache keys
+    /// </summary>
+    public bool HasAssociatedValues => _associatedValues is { Count: > 0 };
+    /// <summary>
+    ///     The dependent keys
+    /// </summary>
+    public HashSet<TKey> Children => _children ??= new HashSet<TKey>();
+
+    /// <summary>
+    ///     Additional values associated with this cache item
+    /// </summary>
+    public IDictionary<object, object> AssociatedValues => _associatedValues ??= new Dictionary<object, object>();
+
+    /// <summary>
+    ///     The policy to check validity
+    /// </summary>
+    public ICachePolicy Policy { get; set; }
+
+    /// <summary>
+    ///     Creates a new instance of <see cref="CacheItem{TKey,TValue}" />
+    /// </summary>
+    /// <param name="key">The key</param>
+    /// <param name="value">The value</param>
+    /// <param name="policy">The policy to check validity</param>
+    public CacheItem(TKey key, TValue value, ICachePolicy policy) {
+        Key = key;
+        Value = value;
+        Policy = policy;
+    }
+
+    /// <summary>
+    ///     Creates a new instance of <see cref="CacheItem{TKey,TValue}" />
+    /// </summary>
+    /// <param name="key">The key</param>
+    /// <param name="value">The value</param>
+    /// <param name="policy">The policy to check validity</param>
+    /// <param name="dependencies">The keys depending on this item</param>
+    public CacheItem(TKey key, TValue value, ICachePolicy policy, IEnumerable<TKey> dependencies) {
+        Key = key;
+        Value = value;
+        Policy = policy;
+        if(dependencies is not null)
+            _children = dependencies as HashSet<TKey> ?? dependencies.ToHashSet();
     }
 }
